d6ab2869b354df47731cb2d53748cb4bbd0d91a3
janithcooray <jvcooray@gmail.com>
2022-06-26 20:48:36 +0530

    added compose -  Update 22-48-26:20:48:36

--- src/sync.js
+++ src/sync.js
@@ -15,23 +15,23 @@ export default class Sync extends Log {
             if (event=="change" || event == "add") {
                 console.log('copy '+path);
                 try {
-                  child_process.execSync('docker exec keells-2022-wp mkdir -p '+dockerpath(getPath(path)));
-                  child_process.execSync('docker cp "'+path+'" keells-2022-wp:'+dockerpath(path));
+                  child_process.execSync('docker exec ' + this.containerName +' mkdir -p '+dockerpath(getPath(path)));
+                  child_process.execSync('docker cp "'+path+'" ' + this.containerName +':'+dockerpath(path));
                 } catch (error) {
                     console.log(getPath(path));
                 }
             }
             if (event=="addDir") {
               console.log('mkdir'  +dockerpath(path));
-              child_process.execSync('docker exec keells-2022-wp mkdir -p '+dockerpath(path));
+              child_process.execSync('docker exec ' + this.containerName +' mkdir -p '+dockerpath(path));
             }
             if (event=="unlink") {
               console.log('remove file ' +dockerpath(path));
-              child_process.execSync('docker exec keells-2022-wp rm -f '+dockerpath(path)+'');
+              child_process.execSync('docker exec ' + this.containerName +' rm -f '+dockerpath(path)+'');
             }
             if (event=="unlinkDir") {
               console.log('remove dir '+dockerpath(path));
-              child_process.execSync('docker exec keells-2022-wp rm -rf '+dockerpath(path)+'');
+              child_process.execSync('docker exec ' + this.containerName +' rm -rf '+dockerpath(path)+'');
             }
           });
     }
