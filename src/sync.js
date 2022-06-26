1d5fe7372d42d5530e51a9db893044ee6750955d
janithcooray <jvcooray@gmail.com>
2022-06-26 20:39:48 +0530

    added compose -  Update 22-39-26:20:39:48

---
+++ src/sync.js
@@ -0,0 +1,9 @@
+import Log from "./abstract/log.js";
+
+export default class Sync extends Log {
+    constructor(){
+        this.containerName =;
+        this.volumePath = ;
+        this.
+    }
+}
