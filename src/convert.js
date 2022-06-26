2d630896f16973cb6c936b241a13a048a4e167df
janithcooray <jvcooray@gmail.com>
2022-06-26 19:41:15 +0530

    Build starting to work -  Update 22-41-26:19:41:15

---
+++ src/convert.js
@@ -0,0 +1,11 @@
+/**
+ * Convert compose file to sync stat
+ */
+import Ops from './abstract/ops.js'
+
+export default class Convert extends Ops{
+    constructor() {
+        super();
+        
+    }
+}
