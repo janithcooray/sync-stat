df13623602c94fa3a621549d63c76ef69214e4ff
janithcooray <jvcooray@gmail.com>
2022-06-27 13:57:46 +0530

    webupdate

--- documentation/src/components/layout.jsx
+++ documentation/src/components/layout.jsx
@@ -1,31 +1,22 @@
-import React, { Children } from "react";
+import React from "react";
 import '../style/main.css'
-import link from 'gatsby';
 import Footer  from "./footer";
 import Sidebar from "./sidebar";
 export default function Layout({children}) {
     return(
-        <html>
-            <head>
-
-            </head>
-            <header>
-            <div class="sidebar">
-                    <Sidebar></Sidebar>
-                </div>
-            </header>
-            <body className="ml-64 mt-24">
-            
-            <div className="mx-auto  max-w-6xl" >
-                <div className=" min-h-screen">
+        <div className="">
+            <div class="w-60 fixed">
+                <Sidebar></Sidebar>
+            </div>
+            <div className="ml-60" >
+                <div className="min-h-screen mx-auto max-w-4xl">
+                    <div className="h-24"></div>
                 { children }
                 </div>
-            <Footer ></Footer>
-            <div className="h-60"></div>
+                <Footer ></Footer>
+                <div className="h-60"></div>
             </div>
-               
-            </body>
- 
-        </html>
+        </div>
     )
 }
+//  
