5a6dfa3c4cf84dcbf3301b83f6d4d3c94be5da2c
janithcooray <jvcooray@gmail.com>
2022-06-28 01:24:13 +0530

    webupdate

---
+++ documentation/src/pages/about.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default function main(){
+    return(
+        <Layout>
+            <h1>about</h1>
+        </Layout>
+    )
+}
