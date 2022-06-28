36dde243db7ceb42bd81fe114d61a88b9c13f831
NOVA <jvcooray@gmail.com>
2022-06-28 09:55:36 +0530

    adding yml support

--- src/abstract/ymlVersion.js
+++ src/abstract/ymlVersion.js
@@ -1,5 +1,20 @@
 import { default as Log } from "./log.js";
 
 export default class YamlVersion extends Log {
+    
+    constructor(){
+
+    }
+
+    /**
+     * Some Yaml functions will be limited to versions
+     */
+    setMinVersion(version){
+        this.version = version;
+    }
+
+    getMinVersion(){
+        return this.version;
+    }
 
 }
