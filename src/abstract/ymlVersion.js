6fde05b4aa30adcd6e0c7878974187996d6e2659
NOVA <jvcooray@gmail.com>
2022-06-28 09:45:49 +0530

    adding yml support

---
+++ src/abstract/ymlVersion.js
@@ -0,0 +1,5 @@
+import { default as Log } from "./log.js";
+
+export default class YamlVersion extends Log {
+
+}
--- src/processYML.js
+++ src/processYML.js
@@ -2,7 +2,24 @@
  * Process YML file
  */
 import Log from "./abstract/log.js";
-
+import yaml from "js-yaml";
 export default class ProcessYML extends Log {
 
+    constructor(){
+
+    }
+
+    /**
+     * Load Yaml File
+     */
+    loadYML(){
+
+    }
+
+    /**
+     * list Containers
+     */
+    getContainers(){
+
+    }
 }
