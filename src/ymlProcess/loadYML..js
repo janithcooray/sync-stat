d14a4316988557d4502dd76d115624adafd01ffe
NOVA <jvcooray@gmail.com>
2022-06-28 10:02:51 +0530

    adding yml support

--- src/ymlProcess/loadYML..js
+++ src/ymlProcess/loadYML..js
@@ -4,6 +4,11 @@ import Log from "../abstract/log";
 export default class LoadYML extends Log {
     constructor(){
         super();
-        
+        let ymlObject = yaml.load(getCompose);
+
     }
+
+    getCompose = () => {
+        return fs.readFileSync( this.getProjectRoot() +'/sync-compose.yml', 'utf8');
+    };
 }
